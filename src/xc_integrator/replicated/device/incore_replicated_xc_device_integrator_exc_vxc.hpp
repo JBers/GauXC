de67a75f1c90c75ac8e192759d2f4fd21eed2abf
Jonathan Bersson <jbersson@uw.edu>
2026-08-27 15:58:06 -0700

    3

--- src/xc_integrator/replicated/device/incore_replicated_xc_device_integrator_exc_vxc.hpp
+++ src/xc_integrator/replicated/device/incore_replicated_xc_device_integrator_exc_vxc.hpp
@@ -545,24 +545,24 @@ void IncoreReplicatedXCDeviceIntegrator<ValueType>::
   };
   std::cout<<"do_zmat_vxc(DEN_S);"<<std::endl;
   do_zmat_vxc(DEN_S);
-  // std::cout<<"do_vxc_ss(DEN_S_SS);"<<std::endl;
-  // do_vxc_ss(DEN_S_SS);
-  // if(not is_rks) {
-  //   std::cout<<"do_zmat_vxc(DEN_Z);"<<std::endl;
-  //   do_zmat_vxc(DEN_Z);
-  //   std::cout<<"do_vxc_ss(DEN_Z_SS);"<<std::endl;
-  //   do_vxc_ss(DEN_Z_SS);
-  //   if(not is_uks) {
-  //     std::cout<<"do_zmat_vxc(DEN_Y);"<<std::endl;
-  //     do_zmat_vxc(DEN_Y);
-  //     std::cout<<"do_vxc_ss(DEN_Y_SS);"<<std::endl;
-  //     do_vxc_ss(DEN_Y_SS);
-  //     std::cout<<"do_zmat_vxc(DEN_X);"<<std::endl;
-  //     do_zmat_vxc(DEN_X);
-  //     std::cout<<"do_vxc_ss(DEN_X_SS);"<<std::endl;
-  //     do_vxc_ss(DEN_X_SS);
-  //   }
-  // } 
+  std::cout<<"do_vxc_ss(DEN_S_SS);"<<std::endl;
+  do_vxc_ss(DEN_S_SS);
+  if(not is_rks) {
+    std::cout<<"do_zmat_vxc(DEN_Z);"<<std::endl;
+    do_zmat_vxc(DEN_Z);
+    std::cout<<"do_vxc_ss(DEN_Z_SS);"<<std::endl;
+    do_vxc_ss(DEN_Z_SS);
+    if(not is_uks) {
+      std::cout<<"do_zmat_vxc(DEN_Y);"<<std::endl;
+      do_zmat_vxc(DEN_Y);
+      std::cout<<"do_vxc_ss(DEN_Y_SS);"<<std::endl;
+      do_vxc_ss(DEN_Y_SS);
+      std::cout<<"do_zmat_vxc(DEN_X);"<<std::endl;
+      do_zmat_vxc(DEN_X);
+      std::cout<<"do_vxc_ss(DEN_X_SS);"<<std::endl;
+      do_vxc_ss(DEN_X_SS);
+    }
+  } 
 
   } // Loop over batches of batches 
 
