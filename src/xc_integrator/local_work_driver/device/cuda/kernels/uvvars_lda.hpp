d0a9ec96e5438c22303c8f5c892a79a0c1dad156
Jonathan Bersson <jbersson@uw.edu>
2026-08-28 11:01:52 -0700

    3

--- src/xc_integrator/local_work_driver/device/cuda/kernels/uvvars_lda.hpp
+++ src/xc_integrator/local_work_driver/device/cuda/kernels/uvvars_lda.hpp
@@ -218,6 +218,8 @@ __global__ void eval_vvar_lda_dks_kern( size_t        ntasks,
 
   if( threadIdx.x == 0 and tid_y < npts ) {
     atomicAdd( den_eval_device   + tid_y, den_reg );
+    printf("den_eval_device, %.10f\n", den_eval_device   + tid_y);
+
   }
   
 }
