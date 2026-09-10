1bc7a56ab911e6dcfb97ca5c1a886f449ea8cafe
Jonathan Bersson <jbersson@uw.edu>
2026-09-10 14:10:23 -0700

    forgor to deref

--- src/xc_integrator/local_work_driver/device/scheme1_base.cxx
+++ src/xc_integrator/local_work_driver/device/scheme1_base.cxx
@@ -1714,7 +1714,7 @@ void AoSScheme1Base::eval_xmat_dks_impl( double fac, XCDeviceData* _data, bool d
       dmat_ptr_SS_i   = static_stack.den_selector(DEN_X_SS_IM);
       std::cout<<"den_select == DEN_S"<<std::endl;
       for( auto i = 0; i < nbf*nbf ; i++) {
-        std::cout<<dmat_ptr_SS_i + i<<std::endl;}
+        std::cout<<*(dmat_ptr_SS_i + i)<<std::endl;}
   }
   if (den_select == DEN_Z) {
       dmat_ptr_SS_dot = static_stack.den_selector(DEN_Z_SS);
