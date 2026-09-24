e17ea6b331899a7468920f485a62400c5264ec8f
Jonathan Bersson <jbersson@uw.edu>
2026-09-24 14:21:53 -0700

    reenable nel spinnel printing on cpu

--- src/xc_integrator/replicated/host/reference_replicated_xc_host_integrator_exc_vxc.hpp
+++ src/xc_integrator/replicated/host/reference_replicated_xc_host_integrator_exc_vxc.hpp
@@ -974,9 +974,9 @@ void ReferenceReplicatedXCHostIntegrator<ValueType>::
   *spin_N_EL = spin_NEL_WORK;
 
   // Prints integrated charge density (N_EL) and integrated mnorm (spin_N_EL)
-  // std::cout<<"N_EL =  "<<*N_EL<<std::endl;
-  // std::cout<<"spin N_EL =  "<<*spin_N_EL<<std::endl;
-  // std::cout<<"EXC =  "<<*EXC<<std::endl;
+  std::cout<<"N_EL =  "<<*N_EL<<std::endl;
+  std::cout<<"spin N_EL =  "<<*spin_N_EL<<std::endl;
+  std::cout<<"EXC =  "<<*EXC<<std::endl;
 
 
 
